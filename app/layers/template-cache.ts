383e891466eb6f8ae34f311891132dd7fc2a86d7
Rasmus Bangsted Pedersen <Rasmus@Rasmuss-MacBook-Pro.local>
2015-10-11 23:51:24 +0200

    removes redundant file

--- app/layers/template-cache.ts
+++
@@ -1,9 +0,0 @@
-declare var require;
-
-var templates = require.context('./layout', true, /\.html$/);
-templates.keys().forEach(templates);
-var foo = 'foo';
-
-console.log(templates);
-
-export { foo as default };
